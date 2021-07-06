47061d7a189eaeedcd14881be55ae82ca11855ba
Chelzea Salvador <chelzeasalvador@gmail.com>
2021-07-06 17:08:42 +0800

    Adding Hero Component

---
+++ components/home-page/hero.js
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import classes from "./hero.module.css";
+import PROFILE_PIC from "../../public/images/site/annabeth.jpeg";
+
+function Hero() {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src={PROFILE_PIC}
+          alt="Annabeth Chase"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi&sbquo; I&rsquo;m Annabeth</h1>
+      <p>I blog about demigod childrens of greek gods and goddesses.</p>
+    </section>
+  );
+}
+
+export default Hero;
