82dd655f835c802648de65b29d6d7541e282c2b8
Chelzea Salvador <chelzeasalvador@gmail.com>
2021-07-07 09:26:56 +0800

    Adding All Posts Page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,42 @@
-function AllPostsPage() {}
+import AllPosts from "../../components/posts/all-posts";
+
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-nextjs",
+    title: "Getting Started NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "Next. js is a React Framework used for front-end development that enables us to use functionalities such as generating static websites and server-side rendering for React-based web applications",
+    date: "2021-05-14",
+  },
+  {
+    slug: "getting-started-nextjs2",
+    title: "Getting Started NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "Next. js is a React Framework used for front-end development that enables us to use functionalities such as generating static websites and server-side rendering for React-based web applications",
+    date: "2021-05-14",
+  },
+  {
+    slug: "getting-started-nextjs3",
+    title: "Getting Started NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "Next. js is a React Framework used for front-end development that enables us to use functionalities such as generating static websites and server-side rendering for React-based web applications",
+    date: "2021-05-14",
+  },
+  {
+    slug: "getting-started-nextjs4",
+    title: "Getting Started NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "Next. js is a React Framework used for front-end development that enables us to use functionalities such as generating static websites and server-side rendering for React-based web applications",
+    date: "2021-05-14",
+  },
+];
+
+function AllPostsPage() {
+  return <AllPosts posts={DUMMY_POSTS} />;
+}
 
 export default AllPostsPage;
