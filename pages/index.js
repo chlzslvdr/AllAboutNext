5830a4577329a37671f86dd87b452d3553c8c714
Chelzea Salvador <chelzeasalvador@gmail.com>
2021-07-09 10:37:48 +0800

    adding metadata to pages

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+import Head from "next/head";
 import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
 import { getFeaturedPosts } from "../lib/posts-util";
@@ -6,6 +7,13 @@ import { getFeaturedPosts } from "../lib/posts-util";
 function HomePage(props) {
   return (
     <Fragment>
+      <Head>
+        <title>AllAboutNext</title>
+        <meta
+          name="description"
+          content="I blog about demigod childrens of greek gods and goddesses."
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </Fragment>
